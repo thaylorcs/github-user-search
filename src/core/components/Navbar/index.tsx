862efe09b9a083a25b7bf14f6bc2dc7a1ad63bde
Thaylor Carvalho <thaylorcs@outlook.com>
2021-02-15 16:12:44 -0300

    Criação da Navbar

---
+++ src/core/components/Navbar/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <h4>BootCamp DevSuperior</h4>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
