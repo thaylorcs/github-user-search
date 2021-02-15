da878447248bb0922126880e559d37a7683742ae
Thaylor Carvalho <thaylorcs@outlook.com>
2021-02-15 20:21:44 -0300

    Criação do container de pesquisa

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Button from '../../core/components/Button';
+import './style.css';
 
 const Search = () => {
     return (
-        <div className="container">
-            <h1>Search</h1>
+        <div className="container-search">
+            <h1>Encontre um perfil no github</h1>
+            <input type="text" placeholder="Usuário Github" />
+            <Button text="Procurar" />
         </div>
     );
 }
